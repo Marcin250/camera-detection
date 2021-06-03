5f5912c427aa72c192131f9af5f8e1d61287d517
Marcin250 <marcinxmurach995@gmail.com>
2021-06-03 11:14:16 -0700

    :pencil: Fix typos

--- app/Capture.py
+++ app/Capture.py
@@ -1,30 +1,41 @@
-import ctypes
 import cv2
 from cv2 import VideoCapture
 from typing import Union
+from numpy import ndarray
 
 
 class Capture:
     def __init__(self, uri: str):
-        self.uri = uri
-        self.connection = self.connect()
+        self.__uri = uri
+        self.__connection = self.__connect()
 
     def __del__(self):
-        self.connection.release()
+        self.__connection.release()
         cv2.destroyAllWindows()
 
-    def connect(self) -> VideoCapture:
-        capture = cv2.VideoCapture(self.uri)
-        capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-        return capture
+    def getConnection(self) -> VideoCapture:
+        return self.__connection
 
-    def getImage(self) -> Union[ctypes.Array, bool]:
-        ret, image = self.connection.read()
+    def reConnect(self) -> VideoCapture:
+        self.__connection = self.__connect()
+
+        return self.__connection
+
+    def getImage(self) -> Union[ndarray, bool]:
+        ret, image = self.__connection.read()
+
         if ret:
             return image
         else:
             return False
 
-    def showImage(self, image: ctypes.Array) -> None:
+    def showImage(self, image) -> int:
         cv2.imshow('IP Camera', image)
-        cv2.waitKey(1)
+
+        return cv2.waitKey(1)
+
+    def __connect(self) -> VideoCapture:
+        capture = cv2.VideoCapture(self.__uri)
+        capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+        return capture
--- app/app.py
+++ app/app.py
@@ -7,7 +7,11 @@ load_dotenv()
 capture = Capture.Capture(os.environ.get('CAMERA_STREAM_URI'))
 
 while True:
-    if capture.connection.isOpened():
+    if capture.getConnection().isOpened():
         image = capture.getImage()
         if ~isinstance(image, bool):
             capture.showImage(image)
+        else:
+            print('Failed to get image')
+    else:
+        print('Failed to connect Camera')
